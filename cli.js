539f4a292eab1cd952c362dfd4c9b9164c061ea0
CodeDotJS <rushforlinux@gmail.com>
2016-05-17 06:49:22 +0530

    || unused

---
+++ cli.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const gooder = require('./index.js');
+
+var s = gooder('https://drive.googcom/file/d/0B3vyNXp6qDWwbXlwZllTd3BuUlk/edit?usp=sharing');
+
+console.log(s);
